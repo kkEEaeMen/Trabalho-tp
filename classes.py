79ed4ffe2fbf526423b3c946622acab78b25755b
Luan Moulais <luanmoulais13@gmail.com>
2022-12-13 20:06:47 -0300

    Modelagem de classes

--- classes.py
+++ classes.py
@@ -10,9 +10,33 @@ class Jogador:
     def getNome(self):
         return self.__nome
 
+    def setAltura(self, altura):
+        self.__altura = altura
+
+    def getAltura(self):
+        return self.__altura
+
+    def setPeso(self, Peso):
+        self.__peso = Peso
+
+    def getPeso(self):
+        return self.__peso
+
+    def setMatricula(self, matricula):
+        self.__matricula = matricula
+
+    def getMatricula(self):
+        return self.__matricula
+ 
     def interface(self):
         return f"O {self.__nome} é gente boa !"
 
+jogador = Jogador()
 
 
-produto = Produto()
+
+
+
+
+
+
