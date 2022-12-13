b95be6b4f3dddfa4ac7f5db745fc84c51f5aefbf
Gabriel Natalli <gabrielnatalli12344321@gmail.com>
2022-12-13 18:47:21 -0300

    Teste commit

--- funcoesPrincipais.py
+++ funcoesPrincipais.py
@@ -1,3 +1,19 @@
+import json
 
-def cadastrar_jogador():
-    print("oi")
+lista = []
+dicionario = {}
+nome_arquivo = "codigo.json"
+
+
+def leitura_dos_dados() -> list:
+
+    arq = open(nome_arquivo, 'r', enconding='utf-8')
+    data = arq.read()
+    return json.loads(data)
+
+def guarda_arquivo(var_lista: list):
+
+    arq = open(nome_arquivo, 'w+', encoding='utf-8')
+    data = json.dumps(var_lista, ident=4)
+    arq.write(data)
+    arq.close()
